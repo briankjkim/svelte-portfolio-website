425f0070aa6eeb4a0cc2ec851278da89a75ded43
briankjkim <kkjin0330@gmail.com>
2025-03-04 14:04:11 -0800

    Change get in touch link from x to linkedin

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -26,7 +26,7 @@
     {/each}
   </div>
   <a
-    href="https://x.com/kynd_b"
+    href="https://www.linkedin.com/in/brian-kj-kim/"
     target="_blank"
     class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
   >
